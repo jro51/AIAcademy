1270235cc2253c6f1b4d01be95dea07b80a298a1
jro51 <julio15.cesar.xd@gmail.com>
2025-06-18 16:16:12 -0500

    Update main.py
    probandooo

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Bienvenido a AI ACADEMY, idea innovadora"
+    return "Bienvenido a AI ACADEMY, idea innovadora, probando desde otro dispositivo"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
