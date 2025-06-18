3dd2f08d3c00280c845ebdc060f48a61ea462e17
Aldo1919xd <calebsamanamudxd@gmail.com>
2025-06-18 16:39:45 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 # main.py
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-    return "Bienvenido a AI ACADEMY, prueba"
+def inicio():
+    return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    app.run(host="0.0.0.0", port=5000)
